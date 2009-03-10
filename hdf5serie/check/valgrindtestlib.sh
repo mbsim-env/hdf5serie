1a705e2c145c7cf60b7d8c4aaab2470f28816149
friedma <friedma@bdaed747-c507-17fa-990b-2c7baa2ee9dd>
2009-03-10 18:08:29 +0000

    Adde valgrind files

---
+++ hdf5serie/check/valgrindtestlib.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+valgrind --help >& /dev/null
+if [ $? -ne 0 ]; then
+  echo "WARNING!"
+  echo "valgrind not found!"
+  echo "Skipping valgrind test of testlib"
+  exit 0
+else
+  valgrind --error-exitcode=1 .libs/lt-testlib
+fi
