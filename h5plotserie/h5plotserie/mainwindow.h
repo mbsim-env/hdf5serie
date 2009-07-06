bbca2e32186b23fb7f53aaade0b07e8d5af3e640
mfoerg <mfoerg@bdaed747-c507-17fa-990b-2c7baa2ee9dd>
2009-07-06 20:37:29 +0000

    initial commit h5plotserie

---
+++ h5plotserie/h5plotserie/mainwindow.h
@@ -0,0 +1,65 @@
+/*
+    h5plotserie - plot the data of a hdf5 file.
+    Copyright (C) 2009 Martin Förg
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef _MAINWINDOW_H_
+#define _MAINWINDOW_H_
+
+#include <QMainWindow>
+
+namespace H5 {
+  class H5File;
+  class Group;
+}
+namespace sdt {
+  class stringstream;
+}
+class QwtPlot;
+class QwtPlotCurve;
+class QTreeWidget;
+class QListWidget;
+class QTreeWidgetItem;
+class QListWidgetItem;
+class QwtPlotZoomer;
+class QCheckBox;
+
+class MainWindow : public QMainWindow {
+
+  Q_OBJECT
+
+  private:
+    QWidget *centralWidget;
+    H5::H5File * file;
+    QwtPlot *myPlot;
+    QwtPlotCurve *curve;
+    QwtPlotZoomer *zoom;
+    QTreeWidget *treeWidget;
+    QListWidget *listWidget;
+
+    void insertChildInTree(H5::Group &grp, QTreeWidgetItem *item);
+    void getPath(QTreeWidgetItem* item, std::stringstream &s, int col);
+  public:
+    MainWindow(std::vector<std::string>& arg);
+
+  public slots:
+    void plot(QListWidgetItem *item);
+    void check(QTreeWidgetItem*, int);
+
+};
+
+#endif
