10b2fe2193830c9a08fc7f983b22c97e19833704
mfoerg <mfoerg@bdaed747-c507-17fa-990b-2c7baa2ee9dd>
2009-07-13 18:57:07 +0000

    Added some functionality (see help of menu)

--- trunk/h5plotserie/h5plotserie/mainwindow.cc
+++ trunk/h5plotserie/h5plotserie/mainwindow.cc
@@ -24,14 +24,20 @@
 #include <QGridLayout>
 #include <QTreeWidget>
 #include <QListWidget>
+#include <QTableWidget>
+#include <QMenuBar>
+#include <QMessageBox>
 #include <qwt_plot.h>
 #include <qwt_plot_curve.h>
 #include <qwt_plot_zoomer.h>
+#include <qwt_legend.h>
+#include <qwt_legend_item.h>
 #include <hdf5serie/simpledataset.h>
 #include <hdf5serie/vectorserie.h>
 #include <QPen>
 #include <QColor>
 #include <sstream>
+#include <iostream>
 
 using namespace H5;
 using namespace std;
@@ -44,19 +50,22 @@ MainWindow::MainWindow(vector<string>& arg) {
   setCentralWidget(centralWidget);
   centralWidget->setLayout(mainlayout);
 
-  QDockWidget *objectListDW=new QDockWidget("Objects",this);
-  objectListDW->setObjectName("Objects");
+  QDockWidget *objectListDW=new QDockWidget("Data",this);
+  //objectListDW->setObjectName("Objects");
   addDockWidget(Qt::LeftDockWidgetArea,objectListDW);
   treeWidget = new QTreeWidget(objectListDW);
-  objectListDW->setWidget(treeWidget);
+  QWidget *objectListWG = new QWidget;
+  objectListDW->setWidget(objectListWG);
+  QHBoxLayout *objectListLO = new QHBoxLayout;
+  objectListWG->setLayout(objectListLO);
+  objectListLO->addWidget(treeWidget);
+  //objectListDW->setWidget(treeWidget);
   treeWidget->setHeaderHidden(true);
   treeWidget->setColumnCount(1);
 
-  QDockWidget *dataListDW=new QDockWidget("Data",this);
-  dataListDW->setObjectName("Data");
-  addDockWidget(Qt::LeftDockWidgetArea,dataListDW);
   listWidget = new QListWidget;
-  dataListDW->setWidget(listWidget);
+  //dataListDW->setWidget(listWidget);
+  objectListLO->addWidget(listWidget);
 
   file = new H5::H5File(arg[0], H5F_ACC_RDONLY);
   QList<QTreeWidgetItem *> items;
@@ -68,18 +77,43 @@ MainWindow::MainWindow(vector<string>& arg) {
     treeWidget->insertTopLevelItem(i,item);
   }
 
-  myPlot = new QwtPlot(QwtText(""), centralWidget);
+  QDockWidget *curveDW=new QDockWidget("Curves",this);
+  //curveDW->setObjectName("Curves");
+  addDockWidget(Qt::LeftDockWidgetArea,curveDW);
+  tableWidget = new QTreeWidget;
+  //tableWidget = new QTableWidget;
+  curveDW->setWidget(tableWidget);
+  tableWidget->setColumnCount(5);
+  QStringList sl;
+  sl << "Number" << "x-label" << "y-label" << "x-path" << "y-path";
+  tableWidget->setHeaderLabels(sl);
 
-  curve = new QwtPlotCurve;
-  curve->setPen(QPen(Qt::red));
-  curve->attach(myPlot);
+  myPlot = new QwtPlot(QwtText(""), centralWidget);
+  //myPlot->setAutoDelete(false);
+  //myPlot->setAutoLegend( true );
+
+  QwtLegend *legend = new QwtLegend;
+  myPlot->insertLegend(legend);
+
+  pen.push_back(QPen(Qt::blue));
+  pen.push_back(QPen(Qt::red));
+  pen.push_back(QPen(Qt::green));
+  pen.push_back(QPen(Qt::magenta));
 
   mainlayout->addWidget(myPlot, 0, 0);
 
+  // help menu
+  menuBar()->addSeparator();
+  QMenu *helpMenu=new QMenu("Help", menuBar());
+  helpMenu->addAction("GUI Help...", this, SLOT(help()));
+  helpMenu->addAction("About OpenMBV...", this, SLOT(about()));
+  menuBar()->addMenu(helpMenu);
+
   zoom = new QwtPlotZoomer(myPlot->canvas());
 
-  QObject::connect(treeWidget, SIGNAL(itemClicked(QTreeWidgetItem*, int)), this, SLOT(check(QTreeWidgetItem*,int)));
+  QObject::connect(treeWidget, SIGNAL(itemClicked(QTreeWidgetItem*, int)), this, SLOT(updateData(QTreeWidgetItem*,int)));
   QObject::connect(listWidget, SIGNAL(itemClicked(QListWidgetItem*)), this, SLOT(plot(QListWidgetItem*)));
+  QObject::connect(tableWidget, SIGNAL(itemDoubleClicked(QTreeWidgetItem*,int)), this, SLOT(detachCurve(QTreeWidgetItem*,int)));
 }
 
 void MainWindow::insertChildInTree(H5::Group &grp, QTreeWidgetItem *item) {
@@ -96,13 +130,7 @@ void MainWindow::insertChildInTree(H5::Group &grp, QTreeWidgetItem *item) {
 	stringstream path; 
 	getPath(item,path,0);
 	path << "/data";
-	vs.open(*file,path.str());
-	QStringList sl;
 	static_cast<TreeWidgetItem*>(child)->setPath(path.str());
-	for(unsigned int i=0; i<vs.getColumns(); i++) {
-	  sl << vs.getColumnLabel()[i].c_str();
-	}
-	static_cast<TreeWidgetItem*>(child)->setStringList(sl);
       }
     }
   }
@@ -116,27 +144,154 @@ void MainWindow::getPath(QTreeWidgetItem* item, stringstream &s, int col) {
 }
 
 void MainWindow::plot(QListWidgetItem* item) {
-  string s = static_cast<TreeWidgetItem*>(treeWidget->currentItem())->getPath();
+  string path = static_cast<TreeWidgetItem*>(treeWidget->currentItem())->getPath();
   int col = listWidget->row(item);
   VectorSerie<double> vs;
-  vs.open(*file,s);
+  vs.open(*file,path);
   vector<double> t = vs.getColumn(0);
   vector<double> q = vs.getColumn(col);
-  curve->setData(&t[0],&q[0],t.size());
-  myPlot->setTitle(s.c_str());
-  myPlot->setAxisTitle(QwtPlot::xBottom,vs.getColumnLabel()[0].c_str());
-  myPlot->setAxisTitle(QwtPlot::yLeft,vs.getColumnLabel()[col].c_str());
+
+  string xlabel = vs.getColumnLabel()[0];
+  string ylabel = vs.getColumnLabel()[col];
+
+  int k=0;
+  if(QApplication::keyboardModifiers() & Qt::ControlModifier) {
+    if(tableWidget->currentItem()) {
+      QwtPlotItemList il = myPlot->itemList();
+      int row = tableWidget->currentIndex().row();
+
+      MyCurve *curve = static_cast<MyCurve*>(il[row]);
+      QwtData* data = curve->data().copy();
+      QwtArrayData* adata = dynamic_cast<QwtArrayData*>(data);
+      curve->setData(&q[0],&adata->yData()[0],q.size());
+      curve->setxPath(path);
+      curve->setyPath(curve->getyPath());
+      curve->setxLabel(ylabel);
+      curve->setyLabel(curve->getyLabel());
+      static_cast<QwtLegendItem*>(myPlot->legend()->find(curve))->setText(QwtText(QString::number(row+1)));
+    }
+  }
+  else if(QApplication::keyboardModifiers() & Qt::ShiftModifier) {
+    k = myPlot->itemList().size();
+    if(k<pen.size()) {
+      MyCurve* newcurve = new MyCurve;
+      newcurve->attach(myPlot);
+      newcurve->setPen(pen[k]);
+      newcurve->setData(&t[0],&q[0],t.size());
+      newcurve->setxPath(path);
+      newcurve->setyPath(path);
+      newcurve->setxLabel(xlabel);
+      newcurve->setyLabel(ylabel);
+      static_cast<QwtLegendItem*>(myPlot->legend()->find(newcurve))->setText(QwtText(QString::number(k+1)));
+    }
+    else {
+      cout << "Error" << endl;
+    }
+  } else
+  {
+    QwtPlotItemList il = myPlot->itemList();
+    for(int i=0; i<il.size(); i++)
+      il[i]->detach();
+
+    MyCurve* newcurve = new MyCurve;
+    newcurve->attach(myPlot);
+    newcurve->setPen(pen[0]);
+    newcurve->setData(&t[0],&q[0],t.size());
+    newcurve->setxPath(path);
+    newcurve->setyPath(path);
+    newcurve->setxLabel(xlabel);
+    newcurve->setyLabel(ylabel);
+    //myPlot->setAxisTitle(QwtPlot::xBottom,xlabel.c_str());
+    //myPlot->setAxisTitle(QwtPlot::yLeft,ylabel.c_str());
+    static_cast<QwtLegendItem*>(myPlot->legend()->find(newcurve))->setText(QwtText(QString::number(1)));
+  }
+  QwtPlotItemList il = myPlot->itemList();
+  //  myPlot->setTitle(path.c_str());
   myPlot->setAxisAutoScale(QwtPlot::xBottom);
   myPlot->setAxisAutoScale(QwtPlot::yLeft);
+
+
   myPlot->replot();
+
   zoom->setZoomBase();
+
+  tableWidget->clear();
+  QStringList sl;
+
+  for(int i=0; i<il.size(); i++) {
+    sl.clear();
+    MyCurve *item = static_cast<MyCurve*>(il[i]);
+    sl << QString::number(i+1) << item->getxLabel().c_str() << item->getyLabel().c_str() << item->getxPath().c_str() << item->getyPath().c_str();
+    QTreeWidgetItem *newItem = new QTreeWidgetItem(sl);
+    tableWidget->addTopLevelItem(newItem);
+  }
+
   vs.close();
 }
 
-void MainWindow::check(QTreeWidgetItem* item, int col) {
+void MainWindow::updateData(QTreeWidgetItem* item, int col) {
   listWidget->clear();
   if( item->text(col) == "data") {
-    listWidget->addItems(static_cast<TreeWidgetItem*>(item)->getStringList());
+    string path = static_cast<TreeWidgetItem*>(item)->getPath();
+    VectorSerie<double> vs;
+    vs.open(*file,path.c_str());
+    QStringList sl;
+    for(unsigned int i=0; i<vs.getColumns(); i++) {
+      sl << vs.getColumnLabel()[i].c_str();
+    }
+    listWidget->addItems(sl);
   }
 }
 
+void MainWindow::detachCurve(QTreeWidgetItem* item, int col) {
+  int row = tableWidget->indexOfTopLevelItem(item);
+  QwtPlotItemList il = myPlot->itemList();
+  il[row]->detach();
+  delete tableWidget->takeTopLevelItem(row);
+  il = myPlot->itemList();
+  for(int i=0; i<il.size(); i++) {
+    MyCurve *curve = static_cast<MyCurve*>(il[i]);
+    curve->setPen(pen[i]);
+    static_cast<QwtLegendItem*>(myPlot->legend()->find(curve))->setText(QwtText(QString::number(i+1)));
+    tableWidget->topLevelItem(i)->setText(0,QString::number(i+1));
+  }
+  myPlot->replot();
+}
+
+void MainWindow::help() {
+  QMessageBox::information(this, "h5plotserie - GUI Help", 
+    "<h1>GUI Help</h1>"
+    "<h2>Actions in data list</h2>"
+    "<ul>"
+    "  <dt>Left-Click</dt><dd> Plot curve (x = first, y = selected) </dd>"
+    "  <dt>Shift+Left-Click</dt><dd> Add curve (x = first, y = selected)</dd>"
+    "  <dt>Ctrl+Left-Click</dt><dd> Change x-axis of current curve (x = selected, y = old) </dd>"
+    "</ul>"
+    "<h2>Actions in curve list</h2>"
+    "  <dt>Left-Click</dt><dd> Choose current curve </dd>"
+    "  <dt>Left-DoubleClick</dt><dd> Remove curve </dd>"
+    );
+}
+
+void MainWindow::about() {
+ QMessageBox::about(this, "About h5plotserie",
+    "<h1>h5plotserie - plot hdf5 file</h1>"
+    "<p>Copyright &copy; Martin Foerg <tt>&lt;mfoerg@user.berlios.de&gt;</tt><p/>"
+    "<p>Licensed under the General Public License (see file COPYING).</p>"
+    "<p>This is free software; see the source for copying conditions.  There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.</p>"
+    "<h2>Authors:</h2>"
+    "<ul>"
+    "  <li>Martin Foerg <tt>&lt;mfoerg@users.berlios.de&gt;</tt> </li>"
+    "  <li>Markus Friedrich <tt>&lt;mafriedrich@users.berlios.de&gt;</tt> </li>"
+    "</ul>"
+    "<h2>This program uses:</h2>"
+    "<ul>"
+    "  <li>'Qt - A cross-platform application and UI framework' by Nokia from <tt>http://www.qtsoftware.com</tt> (License: GPL/LGPL)</li>"
+    "  <li>'Qwt - Qt Widgets for Technical Applications' by Uwe Rathmann from <tt>http://qwt.sourceforge.net</tt> (Licence: Qwt/LGPL)</li>"
+    "  <li>'HDF5Serie - A HDF5 Wrapper for Time Series' by Markus Friedrich from <tt>http://hdf5serie.berlios.de</tt> (License: LGPL)</li>"
+    "  <li>'HDF - Hierarchical Data Format' by The HDF Group from <tt>http://www.hdfgroup.org</tt> (License: NCSA-HDF)</li>"
+    "  <li>...</li>"
+    "</ul>"
+    "<p>A special thanks to all authors of this projects.</p>"
+  );
+}
