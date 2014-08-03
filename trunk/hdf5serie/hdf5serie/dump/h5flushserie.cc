46ffad1cd862f17ed38a755e8da32284dd99919f
friedrich.at.gc@gmail.com <friedrich.at.gc@gmail.com@bdaed747-c507-17fa-990b-2c7baa2ee9dd>
2014-08-03 20:44:20 +0000

    Added a new program h5flushserie which requests a flush of all HDF5Serie files given as parameters. The program wait until the flush is done (or a timeout occurs). Normally this file is not needed to be called since HDF5Serie does this automatically. However it will be usefull when files generated by HDF5Serie are read with other programs/libraries than HDF5Serie (e.g h5plotserie.py)!

---
+++ trunk/hdf5serie/hdf5serie/dump/h5flushserie.cc
@@ -0,0 +1,70 @@
+/* Copyright (C) 2014 Markus Friedrich
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact:
+ *   friedrich.at.gc@googlemail.com
+ *
+ */
+
+#include <iostream>
+#include <hdf5serie/file.h>
+#include <boost/filesystem.hpp>
+#include <boost/shared_ptr.hpp>
+
+using namespace std;
+using namespace H5;
+using namespace boost;
+using namespace boost::filesystem;
+
+int main(int argc, char *argv[]) {
+  try {
+    if(argc==1 || (argc==2 && argv[1]==string("-h"))) {
+      cout<<"Usage: "<<argv[0]<<" [-h| <file> ...]"<<endl;
+      cout<<endl;
+      cout<<"Flush all HDF5Serie files given as arguments."<<endl;
+      cout<<"Waits until all files are flushed successfully or a timeout occures."<<endl;
+      cout<<"Outputs for each file provided as argument, in order, 0 or 1:"<<endl;
+      cout<<"0 = no refresh of this file needed, since the flush was not successfull or no new data are available"<<endl;
+      cout<<"1 = refresh this file, since new data is available"<<endl;
+      return 0;
+    }
+
+    vector<shared_ptr<File> > h5File;
+    for(int i=1; i<argc; ++i)
+      h5File.push_back(make_shared<File>(argv[i], File::read));
+
+    for(vector<shared_ptr<File> >::iterator it=h5File.begin(); it!=h5File.end(); ++it)
+      (*it)->requestWriterFlush();
+    vector<bool> refreshNeeded;
+    refreshNeeded.reserve(h5File.size());
+    for(vector<shared_ptr<File> >::iterator it=h5File.begin(); it!=h5File.end(); ++it)
+      cout<<(*it)->waitForWriterFlush()<<endl;
+
+    return 0;
+  }
+  catch(const Exception &ex) {
+    cout<<"HDF5 error: "<<ex.what()<<endl;
+    return 1;
+  }
+  catch(const std::exception &ex) {
+    cout<<"Exception: "<<ex.what()<<endl;
+    return 1;
+  }
+  catch(...) {
+    cout<<"Unknown exception"<<endl;
+    return 1;
+  }
+}
