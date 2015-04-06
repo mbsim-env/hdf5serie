b23d28e9972bdc5234affd6ec983f7f55bf1fdb7
kgrundl@gmail.com <kgrundl@gmail.com@bdaed747-c507-17fa-990b-2c7baa2ee9dd>
2015-04-06 15:34:45 +0000

    updating h5pyplotserie

--- trunk/h5plotserie/h5pyplotserie/__main__.py
+++ trunk/h5plotserie/h5pyplotserie/__main__.py
@@ -3,6 +3,6 @@ import sys
 import H5PlotSerie
 
 app = QtGui.QApplication(sys.argv)
-h5plot = H5PlotSerie.H5PlotSerie()
+h5plot = H5PlotSerie.H5PlotSerie(sys.argv)
 h5plot.show()
 sys.exit(app.exec_())
