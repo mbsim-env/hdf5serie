58e734574c05921998295524d2bc226213abf7b6
Markus Friedrich <friedrich.at.gc@googlemail.com>
2018-01-17 22:16:12 +0100

    Fix

--- hdf5serie/hdf5serie/check/valgrindtestlib.sh
+++ hdf5serie/hdf5serie/check/valgrindtestlib.sh
@@ -7,11 +7,12 @@ if [ $? -ne 0 ]; then
   echo "Skipping valgrind test of testlib"
   exit 0
 else
+  echo dslfj
   if test -x .libs/lt-testlib; then
     valgrind --error-exitcode=200 --num-callers=150 --leak-check=full --show-reachable=yes .libs/lt-testlib
-  if test -x lt-testlib; then
+  elif test -x ./testlib; then
     valgrind --error-exitcode=200 --num-callers=150 --leak-check=full --show-reachable=yes ./testlib
-  if test -x lt-testlib; then
+  elif test -x ./.libs/testlib; then
     valgrind --error-exitcode=200 --num-callers=150 --leak-check=full --show-reachable=yes ./.libs/testlib
   else
     echo "Unknown install/build stage."
