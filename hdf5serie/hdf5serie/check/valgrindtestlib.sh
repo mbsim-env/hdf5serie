280244ce24ceb7998f72d5f2105b5e0bebca839c
Markus Friedrich <friedrich.at.gc@googlemail.com>
2018-01-17 21:55:23 +0100

    Improved configuration options and library linkage handling

--- hdf5serie/hdf5serie/check/valgrindtestlib.sh
+++ hdf5serie/hdf5serie/check/valgrindtestlib.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 valgrind --help >& /dev/null
 if [ $? -ne 0 ]; then
@@ -9,7 +9,12 @@ if [ $? -ne 0 ]; then
 else
   if test -x .libs/lt-testlib; then
     valgrind --error-exitcode=200 --num-callers=150 --leak-check=full --show-reachable=yes .libs/lt-testlib
-  else
+  if test -x lt-testlib; then
     valgrind --error-exitcode=200 --num-callers=150 --leak-check=full --show-reachable=yes ./testlib
+  if test -x lt-testlib; then
+    valgrind --error-exitcode=200 --num-callers=150 --leak-check=full --show-reachable=yes ./.libs/testlib
+  else
+    echo "Unknown install/build stage."
+    exit 1
   fi
 fi
