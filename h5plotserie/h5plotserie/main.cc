24674b5069fb9e6929e258c33bee11e40d50d127
Markus Friedrich <friedrich.at.gc@googlemail.com>
2020-04-12 22:11:05 +0200

    Fix

--- h5plotserie/h5plotserie/main.cc
+++ h5plotserie/h5plotserie/main.cc
@@ -72,7 +72,7 @@ int main(int argc, char** argv) {
   QCoreApplication::setLibraryPaths(QStringList(QFileInfo(moduleName).absolutePath())); // do not load plugins from buildin defaults
   QApplication app(argc, argv);
   app.setOrganizationName("mbsim-env");
-  app.setApplicationName("h4plotserie");
+  app.setApplicationName("h5plotserie");
   app.setOrganizationDomain("www.mbsim-env.de");
   QSettings::setDefaultFormat(QSettings::IniFormat);
   QLocale::setDefault(QLocale::C);
