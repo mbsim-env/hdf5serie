a332343edf9d1691ed72465bd8ba147a8732d59e
Martin Förg <martin.o.foerg@gmail.com>
2021-05-23 14:39:47 +0200

    Avoid crash when data set does not exist

--- h5plotserie/h5plotserie/plotarea.cc
+++ h5plotserie/h5plotserie/plotarea.cc
@@ -98,14 +98,24 @@ void PlotWindow::plotDataSet(PlotData pd, int penColor) {
   std::vector<double> xVal(rows);
   vs->getColumn(pd.getValue("x-Index").toInt(), xVal);
 
-  vs=h5file->openChildObject<H5::VectorSerie<double> >(pd.getValue("y-Path").toStdString());
+  try {
+    vs=h5file->openChildObject<H5::VectorSerie<double> >(pd.getValue("y-Path").toStdString());
+  }
+  catch(...) {
+    return;
+  }
   std::vector<double> yVal(rows);
   vs->getColumn(pd.getValue("y-Index").toInt(), yVal);
 
   std::vector<double> y2Val(rows);
   bool useY2=false;
   if (pd.getValue("y2-Path").length()>0) {
-    vs=h5file->openChildObject<H5::VectorSerie<double> >(pd.getValue("y2-Path").toStdString());
+    try {
+      vs=h5file->openChildObject<H5::VectorSerie<double> >(pd.getValue("y2-Path").toStdString());
+    }
+    catch(...) {
+      return;
+    }
     vs->getColumn(pd.getValue("y2-Index").toInt(), y2Val);
     useY2=true;
   }
