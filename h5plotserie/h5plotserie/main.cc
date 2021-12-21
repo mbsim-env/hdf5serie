0784d4281a3167a0bee59fbfc7758c87e17063d1
Martin Förg <martin.o.foerg@gmail.com>
2021-12-21 13:58:49 +0100

    Switch to LGPL.
    In order to allow for proprietary plugins we consistently switch to LGPL for all mbsim-env projects.

--- h5plotserie/h5plotserie/main.cc
+++ h5plotserie/h5plotserie/main.cc
@@ -2,19 +2,19 @@
    h5plotserie - plot the data of a hdf5 file.
    Copyright (C) 2010 Markus Schneider
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  This library is free software; you can redistribute it and/or 
+  modify it under the terms of the GNU Lesser General Public 
+  License as published by the Free Software Foundation; either 
+  version 2.1 of the License, or (at your option) any later version. 
+   
+  This library is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+  Lesser General Public License for more details. 
+   
+  You should have received a copy of the GNU Lesser General Public 
+  License along with this library; if not, write to the Free Software 
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
    */
 
 #include <config.h>
@@ -53,7 +53,7 @@ int main(int argc, char** argv) {
         << "This is free software; see the source for copying conditions. There is NO" << endl
         << "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE." << endl
         << "" << endl
-        << "Licensed under the GNU General Public License (GPL)" << endl
+        << "Licensed under the GNU Lesser General Public License (LGPL)" << endl
         << "" << endl
         << "Usage:" << endl
         << "  h5plotserie <file> | <file.h5Layout.xml>" << endl
