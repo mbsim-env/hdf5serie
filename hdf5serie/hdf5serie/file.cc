939bd604067becba125fa20174a883594d3650e4
Markus Friedrich <friedrich@rcstengel.de>
2023-07-31 11:08:11 +0200

    Use a default cacheSize of 100
    This improved write performance a lot with only moderate memory usage

--- hdf5serie/hdf5serie/file.cc
+++ hdf5serie/hdf5serie/file.cc
@@ -40,7 +40,7 @@ namespace H5 {
 
 int File::defaultCompression=1;
 int File::defaultChunkSize=100;
-int File::defaultCacheSize=0;
+int File::defaultCacheSize=100;
 
 namespace Internal {
   // This class is similar to boost::interprocess::scoped_lock but prints debug messages.
